02a0b98683c7a9247e78d20cb03e39e244af2073
Alan Critelli <alancritelli@gmail.com>
2022-10-09 20:31:48 -0300

    add services, stores and contactus
    Adicionado as secções de serviços, unidades e fale conosco.
    Correção de bugs visuais e melhoria na semântica

--- main.js
+++ main.js
@@ -5,3 +5,8 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
+
+new formsapp('634356b50161c14210d144f6', {
+  width: '100%',
+  height: 'formHeight'
+});
